29cfedb9656bd19d6447399651ad0f64f747ff87
KiyotaTakeshi <kiyotatakeshi@KiyotaTakeshis-MacBook-Pro.local>
2022-05-15 22:45:18 +0900

    migration 前に不要なものを削除

--- db/schema.rb
+++ db/schema.rb
@@ -13,11 +13,4 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 20_220_514_061_131) do
-  create_table 'users', charset: 'utf8mb4', collation: 'utf8mb4_0900_ai_ci', force: :cascade do |t|
-    t.string 'name'
-    t.integer 'age'
-    t.string 'hobby'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-  end
 end
--- spec/factories/users.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-FactoryBot.define do
-  factory :user do
-    name { "MyString" }
-    age { 1 }
-    hobby { "MyString" }
-  end
-end
